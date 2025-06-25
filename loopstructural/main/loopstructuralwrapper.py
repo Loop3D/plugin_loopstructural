e695b5f5abe628e32b765bacbbe5692aafede03a
Lachlan Grose <lachlan.grose@monash.edu>
2025-06-25 15:22:25 +1000

    misc

--- loopstructural/main/loopstructuralwrapper.py
+++ loopstructural/main/loopstructuralwrapper.py
@@ -87,7 +87,7 @@ class QgsProcessInputData(ProcessInputData):
                     columnmap['structure_unitname']: 'name',
                     columnmap['dip']: 'dip',
                     columnmap['orientation']: 'strike',
-                } 
+                }
             )[['X', 'Y', 'Z', 'dip', 'strike', 'name']]
             contact_orientations['dip'] = contact_orientations['dip'].astype(float)
             contact_orientations['strike'] = contact_orientations['strike'].astype(float)
@@ -124,7 +124,7 @@ class QgsProcessInputData(ProcessInputData):
                 )
         fault_properties = None
         if len(faults) > 0:
-            
+
             fault_properties = pd.DataFrame(faults)
             fault_properties['fault_name'] = fault_properties['fault_name'].astype(str)
             fault_properties = fault_properties.set_index('fault_name')
