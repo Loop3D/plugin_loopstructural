d4a031281ddb82c3bb4b76a363d4f7cc44e0c0bc
Lachlan Grose <lachlan.grose@monash.edu>
2025-06-25 15:21:16 +1000

    fix: add feature list widget. Shows all features in model for the visualiser

---
+++ loopstructural/gui/visualisation/feature_list_widget.py
@@ -0,0 +1,124 @@
+from PyQt5.QtCore import Qt
+from PyQt5.QtWidgets import (
+    QVBoxLayout,
+    QMenu,
+    QTreeWidget,
+    QTreeWidgetItem,
+    QWidget,
+    QPushButton
+)
+
+class FeatureListWidget(QWidget):
+    def __init__(self, parent=None, *, model_manager=None, viewer=None):
+        super().__init__(parent)
+        self.mainLayout = QVBoxLayout(self)
+        self.treeWidget = QTreeWidget(self)
+        self.treeWidget.setHeaderHidden(True)  # Hide the header
+        self.mainLayout.addWidget(self.treeWidget)
+        self.setLayout(self.mainLayout)
+        self.model_manager = model_manager
+        self.viewer = viewer
+
+        # Add buttons
+        self.addBoundingBoxButton = QPushButton("Add Model Bounding Box", self)
+        self.addFaultSurfacesButton = QPushButton("Add Fault Surfaces", self)
+        self.addStratigraphicSurfacesButton = QPushButton("Add Stratigraphic Surfaces", self)
+
+        # Connect buttons to their respective methods
+        self.addBoundingBoxButton.clicked.connect(self.add_model_bounding_box)
+        self.addFaultSurfacesButton.clicked.connect(self.add_fault_surfaces)
+        self.addStratigraphicSurfacesButton.clicked.connect(self.add_stratigraphic_surfaces)
+
+        # Add buttons to the layout
+        self.mainLayout.addWidget(self.addBoundingBoxButton)
+        self.mainLayout.addWidget(self.addFaultSurfacesButton)
+        self.mainLayout.addWidget(self.addStratigraphicSurfacesButton)
+
+        # Populate the feature list
+        self.update_feature_list()
+        self.model_manager.observers.append(self.update_feature_list)
+    def update_feature_list(self):
+        if not self.model_manager:
+            return
+
+        self.treeWidget.clear()
+        for feature in self.model_manager.features():
+            self.add_feature(feature)
+
+    def add_feature(self, feature):
+        featureItem = QTreeWidgetItem(self.treeWidget)
+        featureItem.setText(0, feature.name)
+
+    def contextMenuEvent(self, event):
+        menu = QMenu(self)
+
+        add_scalar_action = menu.addAction("Add Scalar Field")
+        add_surface_action = menu.addAction("Add Surface")
+        add_vector_action = menu.addAction("Add Vector Field")
+        add_data_action = menu.addAction("Add Data")
+
+        action = menu.exec_(self.mapToGlobal(event.pos()))
+
+        selected_items = self.treeWidget.selectedItems()
+        if not selected_items:
+            return
+
+        feature_name = selected_items[0].text(0)
+
+        if action == add_scalar_action:
+            self.add_scalar_field(feature_name)
+        elif action == add_surface_action:
+            self.add_surface(feature_name)
+        elif action == add_vector_action:
+            self.add_vector_field(feature_name)
+        elif action == add_data_action:
+            self.add_data(feature_name)
+
+    def add_scalar_field(self, feature_name):
+        scalar_field = self.model_manager.model[feature_name].scalar_field()
+        self.viewer.add_mesh(scalar_field.vtk(), name=f'{feature_name}_scalar_field')
+        print(f"Adding scalar field to feature: {feature_name}")
+
+    def add_surface(self, feature_name):
+        surfaces = self.model_manager.model[feature_name].surfaces()
+        for surface in surfaces:
+            self.viewer.add_mesh(surface.vtk(), name=f'{feature_name}_surface')
+        print(f"Adding surface to feature: {feature_name}")
+
+    def add_vector_field(self, feature_name):
+        vector_field = self.model_manager.model[feature_name].vector_field()
+        self.viewer.add_mesh(vector_field.vtk(), name=f'{feature_name}_vector_field')
+        print(f"Adding vector field to feature: {feature_name}")
+
+    def add_data(self, feature_name):
+        data = self.model_manager.model[feature_name].get_data()
+        for d in data:
+            self.viewer.add_mesh(d.vtk(), name=f'{feature_name}_{d.name}') 
+        print(f"Adding data to feature: {feature_name}")
+
+    def add_model_bounding_box(self):
+        if not self.model_manager:
+            print("Model manager is not set.")
+            return
+        bb = self.model_manager.model.bounding_box.vtk()
+        self.viewer.add_mesh(bb, name='model_bounding_box')
+        # Logic for adding model bounding box
+        print("Adding model bounding box...")
+
+    def add_fault_surfaces(self):
+        if not self.model_manager:
+            print("Model manager is not set.")
+            return
+        fault_surfaces = self.model_manager.model.get_fault_surfaces()
+        for surface in fault_surfaces:
+            self.viewer.add_mesh(surface.vtk(), name=f'fault_surface_{surface.name}')
+        print("Adding fault surfaces...")
+
+    def add_stratigraphic_surfaces(self):
+        if not self.model_manager:
+            print("Model manager is not set.")
+            return
+        stratigraphic_surfaces = self.model_manager.model.get_stratigraphic_surfaces()
+        for surface in stratigraphic_surfaces:
+            self.viewer.add_mesh(surface.vtk(), name=f'stratigraphic_surface_{surface.name}')
+        print("Adding stratigraphic surfaces...")
--- loopstructural/gui/visualisation/visualisation_widget.py
+++ loopstructural/gui/visualisation/visualisation_widget.py
@@ -1,3 +1,4 @@
+from PyQt5.QtCore import Qt
 from PyQt5.QtWidgets import (
     QSplitter,
     QVBoxLayout,
@@ -6,14 +7,17 @@ from PyQt5.QtWidgets import (
 
 from .loop_pyvistaqt_wrapper import LoopPyVistaQTPlotter
 from .object_list_widget import ObjectListWidget
+from .feature_list_widget import FeatureListWidget
 
 
 class VisualisationWidget(QWidget):
-    def __init__(self, parent: QWidget = None, mapCanvas=None, logger=None):
+    def __init__(self, parent: QWidget = None, mapCanvas=None, logger=None, model_manager=None):
+
         super().__init__(parent)
         # Load the UI file for Tab 1
         self.mapCanvas = mapCanvas
         self.logger = logger
+        self.model_manager = model_manager
 
         mainLayout = QVBoxLayout(self)
         self.setLayout(mainLayout)
@@ -27,7 +31,15 @@ class VisualisationWidget(QWidget):
         # Create the viewer
         self.plotter = LoopPyVistaQTPlotter(parent)
         # self.plotter.add_axes()
-        
+
         self.objectList = ObjectListWidget(viewer=self.plotter)
-        splitter.addWidget(self.objectList)
+
+        # Modify layout to stack object list and feature list vertically
+        sidebarSplitter = QSplitter(Qt.Vertical, self)
+        sidebarSplitter.addWidget(self.objectList)
+
+        # Create the feature list widget
+        self.featureList = FeatureListWidget(model_manager=self.model_manager, viewer=self.plotter)
+        sidebarSplitter.addWidget(self.featureList)
+        splitter.addWidget(sidebarSplitter)
         splitter.addWidget(self.plotter)
