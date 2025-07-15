433eabbc717ebb231dd6c75724efc7620b9a86b3
Lachlan Grose <lachlan.grose@monash.edu>
2025-07-15 13:36:54 +1000

    add regularisation/npw/cpw

--- loopstructural/gui/modelling/feature_details_panel.py
+++ loopstructural/gui/modelling/feature_details_panel.py
@@ -1,11 +1,11 @@
 from PyQt5.QtWidgets import (
-    QWidget, QVBoxLayout, QSlider, QLabel, QDoubleSpinBox, QCheckBox, QFormLayout
+    QWidget, QVBoxLayout, QSlider, QLabel, QDoubleSpinBox, QCheckBox, QFormLayout, QComboBox
 )
 from PyQt5.QtCore import Qt
 from PyQt5.QtWidgets import QScrollArea
 from LoopStructural.utils import normal_vector_to_strike_and_dip
 class BaseFeatureDetailsPanel(QWidget):
-    def __init__(self, parent=None):
+    def __init__(self, parent=None,*, feature=None):
         super().__init__(parent)
 
         # Create a scroll area for horizontal scrolling
@@ -27,9 +27,67 @@ class BaseFeatureDetailsPanel(QWidget):
         # Set the main layout
         self.setLayout(mainLayout)
 
+        ## define interpolator parameters
+        # Regularisation slider
+        self.regularisation_slider = QSlider(Qt.Horizontal)
+        self.regularisation_slider.setRange(0, 100)
+        self.regularisation_slider.setValue(1)
+        self.regularisation_label = QLabel("Regularisation: 1")
+        self.regularisation_slider.valueChanged.connect(
+            lambda value: self.regularisation_label.setText(f"Regularisation: {value}")
+        )
+        # self.regularisation_slider.valueChanged.connect(
+        #     lambda value: feature.builder.foliation_parameters.__setitem__('regularisation', value)
+        # )
+        self.regularisation_slider.valueChanged.connect(
+            lambda value: feature.builder.update_build_arguments({'regularisation': value})
+        )
+        self.cpw_slider = QSlider(Qt.Horizontal)
+        self.cpw_slider.setRange(0, 100)
+        self.cpw_slider.setValue(1)
+        self.cpw_label = QLabel("Value point weight: 1")
+        self.cpw_slider.valueChanged.connect(
+            lambda value: self.cpw_label.setText(f"Value point weight: {value}")
+        )
+        self.cpw_slider.valueChanged.connect(
+            lambda value: feature.builder.update_build_arguments({'cpw':value})
+        )
+        self.npw_slider = QSlider(Qt.Horizontal)
+        self.npw_slider.setRange(0, 100)
+        self.npw_slider.setValue(1)
+        self.npw_label = QLabel("Normal vector weight: 1")
+        self.npw_slider.valueChanged.connect(
+            lambda value: self.npw_label.setText(f"Normal vector weight: {value}")
+        )
+        self.npw_slider.valueChanged.connect(
+            lambda value: feature.builder.update_build_arguments({'npw':value})
+        )
+        self.interpolator_type_label = QLabel("Interpolator Type:")
+        self.interpolator_type_combo = QComboBox()
+        self.interpolator_type_combo.addItems(["FDI", "PLI", "surfe"])
+
+        self.n_elements_spinbox = QDoubleSpinBox()
+        self.n_elements_spinbox.setRange(100, 1000000)
+        self.n_elements_spinbox.setValue(5000)
+        self.n_elements_spinbox.setPrefix("Number of Elements: ")
+        
+        self.n_elements_spinbox.valueChanged.connect(lambda value: feature.builder.update_build_arguments({'nelements  ': value}))
+
+        # Form layout for better organization
+        form_layout = QFormLayout()
+        form_layout.addRow(self.interpolator_type_label, self.interpolator_type_combo)
+        form_layout.addRow("Number of Elements:", self.n_elements_spinbox)
+        form_layout.addRow(self.regularisation_label, self.regularisation_slider)
+        form_layout.addRow(self.cpw_label, self.cpw_slider)
+        form_layout.addRow(self.npw_label, self.npw_slider)
+
+        
+
+        self.layout.addLayout(form_layout)
+
 class FaultFeatureDetailsPanel(BaseFeatureDetailsPanel):
     def __init__(self, parent=None,*, fault=None):
-        super().__init__(parent)
+        super().__init__(parent,feature=fault)
         if fault is None:
             raise ValueError("Fault must be provided.")
         self.fault = fault
@@ -40,17 +98,15 @@ class FaultFeatureDetailsPanel(BaseFeatureDetailsPanel):
                                 'minor_axis_length': fault.fault_minor_axis,
                                 'intermediate_axis_length': fault.fault_intermediate_axis,
                                 'dip': dip,
+                                # 'pitch'
                                 # 'enabled': fault.fault_enabled
                                 }
 
         # Fault displacement slider
-        self.displacement_slider = QSlider(Qt.Horizontal)
-        self.displacement_slider.setRange(0, 1000)  # Example range
-        self.displacement_slider.setValue(self.fault.displacement)
-        self.displacement_label = QLabel(f"Fault Displacement: {self.fault.displacement}")
-        self.displacement_slider.valueChanged.connect(
-            lambda value: self.displacement_label.setText(f"Fault Displacement: {value}")
-        )
+        self.displacement_spinbox = QDoubleSpinBox()
+        self.displacement_spinbox.setRange(0, 1000000)  # Example range
+        self.displacement_spinbox.setValue(self.fault.displacement)
+        self.displacement_label = QLabel(f"Fault Displacement:")
         self.displacement_slider.valueChanged.connect(
             lambda value: self.fault_parameters.__setitem__('displacement', value)
         )
@@ -86,13 +142,15 @@ class FaultFeatureDetailsPanel(BaseFeatureDetailsPanel):
         self.dip_spinbox.valueChanged.connect(
             lambda value: self.fault_parameters.__setitem__('dip', value)
         )
+        # self.dip_spinbox.valueChanged.connect(
+            
         # Enabled field
         # self.enabled_checkbox = QCheckBox("Enabled")
         # self.enabled_checkbox.setChecked(False)
 
         # Form layout for better organization
         form_layout = QFormLayout()
-        form_layout.addRow(self.displacement_label, self.displacement_slider)
+        form_layout.addRow(self.displacement_label, self.displacement_spinbox)
         form_layout.addRow("Major Axis Length:", self.major_axis_spinbox)
         form_layout.addRow("Minor Axis Length:", self.minor_axis_spinbox)
         form_layout.addRow("Intermediate Axis Length:", self.intermediate_axis_spinbox)
@@ -104,33 +162,8 @@ class FaultFeatureDetailsPanel(BaseFeatureDetailsPanel):
 
 class FoliationFeatureDetailsPanel(BaseFeatureDetailsPanel):
     def __init__(self, parent=None,*, feature=None):
-        super().__init__(parent)
+        super().__init__(parent, feature=feature)
 
-        # Foliation thickness slider
-        self.thickness_slider = QSlider(Qt.Horizontal)
-        self.thickness_slider.setRange(0, 100)  # Example range
-        self.thickness_slider.setValue(0)
-        self.thickness_label = QLabel("Foliation Thickness: 0")
-        self.thickness_slider.valueChanged.connect(
-            lambda value: self.thickness_label.setText(f"Foliation Thickness: {value}")
-        )
-
-        # Foliation orientation
-        self.orientation_spinbox = QDoubleSpinBox()
-        self.orientation_spinbox.setRange(0, 360)  # Orientation angle range
-        self.orientation_spinbox.setValue(0)
-        self.orientation_spinbox.setPrefix("Orientation: ")
-
-        # Enabled field
-        self.enabled_checkbox = QCheckBox("Enabled")
-        self.enabled_checkbox.setChecked(False)
-
-        # Form layout for better organization
-        form_layout = QFormLayout()
-        form_layout.addRow(self.thickness_label, self.thickness_slider)
-        form_layout.addRow("Orientation:", self.orientation_spinbox)
-        form_layout.addRow("Enabled:", self.enabled_checkbox)
-
-        self.layout.addLayout(form_layout)
+        
         # Remove redundant layout setting
         # self.setLayout(self.layout)
